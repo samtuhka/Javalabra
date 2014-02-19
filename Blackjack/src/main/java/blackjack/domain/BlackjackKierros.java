285b150fc5780a72a796d4fd1e258569bf555938
unknown <samuel.tuhkanen@helsinki.fi>
2014-02-19 21:49:41 +0200

    lisaa saatoa

--- Blackjack/src/main/java/blackjack/domain/BlackjackKierros.java
+++ Blackjack/src/main/java/blackjack/domain/BlackjackKierros.java
@@ -63,7 +63,7 @@ public class BlackjackKierros {
         if (pelaaja.getKasi().busted()) {
             return false;
         }
-        if (pelaaja.getPisteet() < jakaja.getPisteet() && jakaja.getPisteet()<=21) {
+        if (pelaaja.getPisteet() < jakaja.getPisteet() && jakaja.getPisteet() <= 21) {
             return false;
         } else if (pelaaja.getPisteet() == 21 && jakaja.getPisteet() == 21 && pelaaja.getKasi().getKorttienMaara() == 2 && jakaja.getKasi().getKorttienMaara() != 2) {
             return true;
@@ -74,11 +74,24 @@ public class BlackjackKierros {
         }
     }
 
+    public boolean tasaPeli() {
+        if (pelaaja.getKasi().onBlackjack() && jakaja.getKasi().onBlackjack()) {
+            return true;
+        }
+        if (!pelaaja.getKasi().onBlackjack() && !jakaja.getKasi().onBlackjack() && pelaaja.getPisteet() == 21 && jakaja.getPisteet() == 21) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /**
      * Lisaa tai poistaa pelaajan kassasta panoksen.
      */
     public void kierroksenLoppu() {
-        if (pelaajaVoittaa()) {
+        if (tasaPeli()) {
+            return;
+        } else if (pelaajaVoittaa()) {
             pelaaja.lisaaKassaan(panos);
         } else {
             pelaaja.poistaKassasta(panos);
